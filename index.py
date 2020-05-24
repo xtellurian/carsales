4bdb8afa3921c2c479ce19ffa1c176c99630cf75
jonopye <jonopye@gmail.com>
2020-05-24 13:08:10 +1000

    write to CSV

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
+import csv
+
 from parse import parse
+from model import ItemListElement
 
 sales = parse(100)
 
@@ -12,3 +15,15 @@ for e in sales.main_entity.item_list_element:
     print(f'ODO: {e.item.mileage_from_odometer.value}')
     print(f'PRICE: {e.item.offers.price}')
     print('------------------------')
+
+def save_to_csv(cars: [ItemListElement]):
+    with open("test.csv","w",newline="") as csvfile:
+        writer=csv.writer(csvfile,delimiter=",")
+        for car in cars:
+            print(f'{car.item.name},{car.item.name[0:4]},{car.item.mileage_from_odometer.value},{car.item.offers.price}')
+            line=[car.item.name,car.item.name[0:4],car.item.mileage_from_odometer.value,car.item.offers.price]
+            writer.writerow(line)
+   
+
+
+save_to_csv(sales.main_entity.item_list_element)
